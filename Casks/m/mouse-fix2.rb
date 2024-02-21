86845812fab760bbf1927676c5ca55e164c2257b
AJ Cloete <ajcloete@gmail.com>
2024-02-21 12:47:27 +0200

    Update mouse-fix2.rb

--- Casks/m/mouse-fix2.rb
+++ Casks/m/mouse-fix2.rb
@@ -1,22 +1,25 @@
-cask "mouse-fix2" do
+cask "mac-mouse-fix-free" do
   version "2.2.3"
   sha256 "6f2eea2403b97e7d045d364b28bdd3f6a85a8e3c70470e94ab4f22add2e4ce7d"
 
-  url "https://github.com/noah-nuebling/mac-mouse-fix/releases/download/#{version}/MacMouseFixApp.zip"
-  name "mouse-fix2"
-  desc ""
-  homepage ""
+  url "https://github.com/noah-nuebling/mac-mouse-fix/releases/download/#{version}/MacMouseFixApp.zip",
+      verified: "github.com/noah-nuebling/mac-mouse-fix/"
+  name "Mac Mouse Fix Free"
+  desc "Mouse utility to add gesture functions and smooth scrolling to 3rd party mice"
+  homepage "https://noah-nuebling.github.io/mac-mouse-fix-website/"
 
-  # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
-    url ""
-    strategy ""
+    skip "Last free version is 2.2"
   end
+
+  auto_updates false
 
   depends_on macos: ""
 
-  app "Mouse Fix2"
+  app "Mac Mouse Fix Free.app"
 
-  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
-  zap trash: ""
+  zap trash: [
+    "~/Library/Application Support/com.nuebling.mac-mouse-fix",
+    "~/Library/LaunchAgents/com.nuebling.mac-mouse-fix.helper.plist",
+  ]
 end
