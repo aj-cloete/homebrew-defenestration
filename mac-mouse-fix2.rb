6d40156be0014abfad826ddcd4bd883db0405c08
AJ Cloete <ajcloete@gmail.com>
2024-02-21 11:21:27 +0200

    Update mac-mouse-fix2.rb

--- mac-mouse-fix2.rb
+++ mac-mouse-fix2.rb
@@ -1,5 +1,6 @@
 cask "mac-mouse-fix2" do
   version "2.3.3"
+  sha256 no_check
 
   url "https://github.com/aj-cloete/mac-mouse-fix/releases/download/#{version}/MacMouseFixApp.zip",
       verified: "github.com/aj-cloete/mac-mouse-fix/"
